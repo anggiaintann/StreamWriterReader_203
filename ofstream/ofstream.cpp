97f1a094c946a10d68ce1f5287dd605a4475d8d7
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-06-19 14:58:14 +0700

    jika file ada

--- ofstream/ofstream.cpp
+++ ofstream/ofstream.cpp
@@ -30,4 +30,11 @@ int main() {
 	ifstream infile;
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open.open())
+	{
+
+	}
 }
